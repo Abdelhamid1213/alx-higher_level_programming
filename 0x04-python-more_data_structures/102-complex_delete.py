c04ba6f2529124d47e8ae0325f35fd9477fcb648
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-10-13 00:16:27 +0100

    Advanced

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        tmp_dic = a_dictionary.copy()
+        for key in tmp_dic.keys():
+            if tmp_dic[key] == value:
+                a_dictionary.pop(key)
+
+
+complex_delete = __import__('102-complex_delete').complex_delete
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+new_dict = complex_delete(a_dictionary, 'C')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = complex_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
