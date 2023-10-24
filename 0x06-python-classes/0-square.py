b20ac940be779ccd1d56673aac8f35ace96f8f76
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-10-24 11:10:52 +0100

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""A simple square class
-
-"""
+"""Square Class Module"""
 
 
 class Square:
+    """A class representing a square."""
+
     def __init__(self):
+        """Initialize a square with default attributes."""
         pass
