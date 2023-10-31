4f235001adec3b55777c04e66890d0dab89b633f
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-10-31 07:52:48 +0100

    Task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,20 +14,6 @@ class Rectangle():
         type(self).number_of_instances += 1
 
     @property
-    def width(self):
-        """Get the width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Set the width"""
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
-
-    @property
     def height(self):
         """Get the height"""
         return self.__height
@@ -41,6 +27,20 @@ class Rectangle():
             raise ValueError('height must be >= 0')
         self.__height = value
 
+    @property
+    def width(self):
+        """Get the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
     def area(self):
         """Get Area of Rectangle"""
         return self.__height * self.__width
@@ -50,6 +50,22 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__height * 2) + (self.__width * 2)
+    
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Returns a new Rectangle instance with width == height == size"""
+        return (cls(size, size))
 
     def __str__(self):
         """Return sting to be printed"""
@@ -70,19 +86,3 @@ class Rectangle():
         """Print a message for every deletion of a Rectangle."""
         print('Bye rectangle...')
         type(self).number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Returns the biggest rectangle based on the area"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError('rect_1 must be an instance of Rectangle')
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError('rect_2 must be an instance of Rectangle')
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """Returns a new Rectangle instance with width == height == size"""
-        return (cls(size, size))
