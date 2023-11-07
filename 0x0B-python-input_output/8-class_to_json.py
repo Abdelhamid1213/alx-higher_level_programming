dc9d5d90231375b230a0b336af99e2256703e599
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-11-07 11:41:00 +0100

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,7 +4,5 @@ import json
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization
-    of an object"""
-    return json.dumps(obj.__dict__)
+    """returns the dictionary description with simple data structure"""
+    return obj.__dict__
