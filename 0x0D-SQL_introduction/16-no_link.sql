c7310ab41a4e01e6fb1428d0bba6176a66f3cbcf
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-12-12 20:45:37 +0100

    Tasks

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Retrieve the "score" and "name" columns from the "second_table" where the "name" is not NULL and not an empty string.
-SELECT `score`, `name` FROM `second_table` WHERE `name` IS NOT NULL AND `name` != '';
+SELECT `score`, `name` FROM `second_table` WHERE `name` != '' ORDER BY `score` DESC;
