ce5e24bc0ed6bafe5e2a62569e52f3003d5e5278
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-12-12 19:51:46 +0100

    Tasks

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Retrieve all records from the "first_table" in the "hbtn_0c_0" database where the "id" is equal to 89.
-SELECT * FROM `first_table` WHERE id = 89;
+SELECT COUNT(*) FROM `first_table` WHERE id = 89;
