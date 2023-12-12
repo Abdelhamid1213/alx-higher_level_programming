0b57049ce748cd1c390cd8d4e5a70b365d493d2b
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-12-12 19:58:49 +0100

    Tasks

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
--- Retrieve the "score" and "id" columns from the "second_table" in descending order based on the "score" column.
-SELECT score, id FROM second_table ORDER BY score DESC;
+-- Retrieve the "score" and "name" columns from the "second_table" in descending order based on the "score" column.
+SELECT score, name FROM second_table ORDER BY score DESC;
