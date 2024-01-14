66c403b34fa418805afb18ef2d22d7334d691849
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-01-14 09:11:29 +0100

    Tasks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL server and retrieves and displays states from the 'states' table.
+The results are sorted in ascending order by states.id.
+"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract MySQL credentials from command-line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
+def select_states(username, password, database):
+    """
+    Connect to MySQL server and retrieve states from the 'states' table.
+    
+    :param username: MySQL username
+    :param password: MySQL password
+    :param database: MySQL database name
+    """
     # Connect to MySQL server on localhost at port 3306
     db = MySQLdb.connect(
         host="localhost",
@@ -36,3 +40,18 @@ if __name__ == "__main__":
     # Close cursor and database connection
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    """
+    Main function to handle command-line arguments and execute the script.
+    """
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract MySQL credentials from command-line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Call the select_states function
+    select_states(username, password, database)
