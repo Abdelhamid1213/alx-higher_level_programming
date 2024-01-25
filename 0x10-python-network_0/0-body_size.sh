572fc6ae92814a80708a3f798f72bf939b05454d
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-01-25 16:07:09 +0100

    Tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response.
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-size=$(curl -sI "$url" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n')
-
-if [ -n "$size" ]; then
-    echo "$size"
-else
-    echo "Unable to determine the size of the response body"
-fi
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 # This script is used to retrieve the body of a response from a given URL.
 # It sends a GET request to the URL and prints the body of the response.
-
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-if [ "$response" -eq 200 ]; then
-    curl -s "$1"
-else
-    echo "Error: Response status code is not 200"
-fi
+curl -sL "$1"
