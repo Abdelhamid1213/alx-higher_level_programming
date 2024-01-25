20250afa19960e001a001f2e3c92198d1d1d820a
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-01-25 16:40:16 +0100

    Tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script is used to retrieve the body of a response from a given URL.
-# It sends a GET request to the URL and prints the body of the response.
+# This script is used to retrieve the body of a response from a given URL. It sends a GET request to the URL and prints the body of the response.
 curl -sL "$1"
