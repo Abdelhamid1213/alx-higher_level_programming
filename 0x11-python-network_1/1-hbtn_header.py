a2230899708a36bd13b0c62726388964d66b99db
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-01-26 18:18:06 +0100

    Tasks

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+"""A script that sends a request to a URL and prints the value of the 'X-Request-Id' header in the response.
+
+Usage: python3 1-hbtn_header.py <URL>
+"""
+
+if len(sys.argv) != 2:
+    print(f'Usage: {sys.argv[0]} <URL>')
+    quit()
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    x_request_id = response.getheader('X-Request-Id')
+
+print(x_request_id)
