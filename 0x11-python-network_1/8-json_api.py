701223efda03af01b91b814ac6a2ff04c2cffe8f
Abdelhamid <abdelhamidelmaazi@gmail.com>
2024-01-26 20:08:00 +0100

    Tasks

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+"""
+This script sends a POST request to a specified URL with a search query
+parameter.
+
+It expects a JSON response and prints the ID and name of the result if found,
+or "No result" if no result is found. If the response is not a valid JSON,
+it prints "Not a valid JSON".
+"""
+
+url = "http://0.0.0.0:5000/search_user"
+data = {
+    'q': (sys.argv[1] if len(sys.argv) == 2 else "")
+}
+
+with requests.post(url, data) as response:
+    try:
+        json_data = response.json()
+        if json_data:
+            print(f"[{json_data['id']}] {json_data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
